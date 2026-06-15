fc04663cdba65e5f70b7a8dd26c3c0f532240bcd
Abtin Mogharabin <atbinmogharabin@gmail.com>
2026-06-15 09:45:04 +0200

    added the website

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# icml26-fairssl.github.io
+# FairSSL Website
+
+Source code for the FairSSL project website.
+
+Website: https://icml26-fairssl.github.io/
+
+Paper: `static/pdfs/FairSSL_paper.pdf`  
+Poster: `static/poster/FairSSL_poster.pdf`  
+Code: https://github.com/abtinmU/FairSSL
+
+This is a static GitHub Pages site. To publish it, place `index.html`, `README.md`, and the `static/` directory at the root of the `icml26-fairssl.github.io` repository and deploy from the `main` branch root.
+
+Website design adapted from Nerfies.
---
+++ static/js/index.js
@@ -0,0 +1,38 @@
+window.HELP_IMPROVE_VIDEOJS = false;
+
+$(document).ready(function() {
+    // Check for click events on the navbar burger icon.
+    $(".navbar-burger").click(function() {
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+    });
+
+    var options = {
+      slidesToScroll: 1,
+      slidesToShow: 3,
+      loop: true,
+      infinite: true,
+      autoplay: false,
+      autoplaySpeed: 3000,
+    };
+
+    // Initialize all divs with carousel class.
+    var carousels = bulmaCarousel.attach('.carousel', options);
+
+    for(var i = 0; i < carousels.length; i++) {
+      carousels[i].on('before:show', state => {
+        console.log(state);
+      });
+    }
+
+    var element = document.querySelector('#my-element');
+    if (element && element.bulmaCarousel) {
+      element.bulmaCarousel.on('before-show', function(state) {
+        console.log(state);
+      });
+    }
+
+    if (document.querySelector('.slider')) {
+      bulmaSlider.attach();
+    }
+});
